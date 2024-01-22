c7d46825d3c9e8e13b2fb33f611d0efd6837c06a
anuksalgado <118955816+anuksalgado@users.noreply.github.com>
2024-01-22 19:53:46 +0800

    front end

--- Website-files/tailwind.config.js
+++ Website-files/tailwind.config.js
@@ -1,25 +1,45 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./public/**/*.html", // Include HTML files in the src directory
-    "./public/**/*.css", // Include HTML files in the src directory
-    "./public/**/*.js",   // Include JavaScript files in the src directory
+    "./public/**/*.html",
+    "./public/**/*.js", 
     "./src/**/*.css",
   ],
   theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'white': '#ffffff',
-      'purple': '#3f3cbb',
-      'midnight': '#121063',
-      'metal': '#565584',
-      'tahiti': '#3ab7bf',
-      'silver': '#ecebff',
-      'bubble-gum': '#ff77e9',
-      'bermuda': '#78dcca',
+    extend: {
+      fontFamily:{
+        playfair: ['PlayFair Display', 'serif'],
+        playfairExtraBold: ['PlayFair Display', 'serif', 'ExtraBold']
+      },
+      colors: {
+        'custom-yellow':'#BAA333',
+        'cream-white' :'#FFF8E1',
+        cyan: {
+          50: '#e0f7fa',
+          100: '#b2ebf2',
+          200: '#80deea',
+          300: '#4dd0e1',
+          400: '#26c6da',
+          500: '#00bcd4',
+          600: '#00acc1',
+          700: '#0097a7',
+          800: '#00838f',
+          900: '#006064',
+        },
+        blue: {
+          50: '#e3f2fd',
+          100: '#bbdefb',
+          200: '#90caf9',
+          300: '#64b5f6',
+          400: '#42a5f5',
+          500: '#2196f3',
+          600: '#1e88e5',
+          700: '#1976d2',
+          800: '#1565c0',
+          900: '#0d47a1',
+        },
+      },
     },
-    extend: {},
   },
   plugins: [],
 }
