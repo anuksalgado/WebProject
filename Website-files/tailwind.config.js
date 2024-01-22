bc8af7764d4ce2dbbbd5b85c03a23db51635c9ff
anuksalgado <118955816+anuksalgado@users.noreply.github.com>
2024-01-22 19:17:14 +0800

    Added tailwind to Web files

---
+++ Website-files/tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/**/*.html", // Include HTML files in the src directory
+    "./public/**/*.css", // Include HTML files in the src directory
+    "./public/**/*.js",   // Include JavaScript files in the src directory
+    "./src/**/*.css",  // Include CSS files in the src directory
+  ],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
